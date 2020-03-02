6af1b920de133adb32c652468f3dbf0139337a46
Igor Maliy <imaliy.filsh@gmail.com>
2020-03-02 12:11:11 +0200

    Initial commit

---
+++ dist/main.umd.js
@@ -0,0 +1,229 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global._____ = {}));
+}(this, (function (exports) { 'use strict';
+
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+
+  var script = {
+    name: 'ui-brand-card',
+    methods: {
+      handleHover: function handleHover() {
+        console.log(1);
+      }
+    }
+  };
+
+  function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+      if (typeof shadowMode !== 'boolean') {
+          createInjectorSSR = createInjector;
+          createInjector = shadowMode;
+          shadowMode = false;
+      }
+      // Vue.extend constructor export interop.
+      var options = typeof script === 'function' ? script.options : script;
+      // render functions
+      if (template && template.render) {
+          options.render = template.render;
+          options.staticRenderFns = template.staticRenderFns;
+          options._compiled = true;
+          // functional template
+          if (isFunctionalTemplate) {
+              options.functional = true;
+          }
+      }
+      // scopedId
+      if (scopeId) {
+          options._scopeId = scopeId;
+      }
+      var hook;
+      if (moduleIdentifier) {
+          // server build
+          hook = function (context) {
+              // 2.3 injection
+              context =
+                  context || // cached call
+                      (this.$vnode && this.$vnode.ssrContext) || // stateful
+                      (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+              // 2.2 with runInNewContext: true
+              if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                  context = __VUE_SSR_CONTEXT__;
+              }
+              // inject component styles
+              if (style) {
+                  style.call(this, createInjectorSSR(context));
+              }
+              // register component module identifier for async chunk inference
+              if (context && context._registeredComponents) {
+                  context._registeredComponents.add(moduleIdentifier);
+              }
+          };
+          // used by ssr in case component is cached and beforeCreate
+          // never gets called
+          options._ssrRegister = hook;
+      }
+      else if (style) {
+          hook = shadowMode
+              ? function (context) {
+                  style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+              }
+              : function (context) {
+                  style.call(this, createInjector(context));
+              };
+      }
+      if (hook) {
+          if (options.functional) {
+              // register for functional component in vue file
+              var originalRender = options.render;
+              options.render = function renderWithStyleInjection(h, context) {
+                  hook.call(context);
+                  return originalRender(h, context);
+              };
+          }
+          else {
+              // inject component registration as beforeCreate hook
+              var existing = options.beforeCreate;
+              options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+          }
+      }
+      return script;
+  }
+
+  var isOldIE = typeof navigator !== 'undefined' &&
+      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
+  function createInjector(context) {
+      return function (id, style) { return addStyle(id, style); };
+  }
+  var HEAD;
+  var styles = {};
+  function addStyle(id, css) {
+      var group = isOldIE ? css.media || 'default' : id;
+      var style = styles[group] || (styles[group] = { ids: new Set(), styles: [] });
+      if (!style.ids.has(id)) {
+          style.ids.add(id);
+          var code = css.source;
+          if (css.map) {
+              // https://developer.chrome.com/devtools/docs/javascript-debugging
+              // this makes source maps inside style tags work properly in Chrome
+              code += '\n/*# sourceURL=' + css.map.sources[0] + ' */';
+              // http://stackoverflow.com/a/26603875
+              code +=
+                  '\n/*# sourceMappingURL=data:application/json;base64,' +
+                      btoa(unescape(encodeURIComponent(JSON.stringify(css.map)))) +
+                      ' */';
+          }
+          if (!style.element) {
+              style.element = document.createElement('style');
+              style.element.type = 'text/css';
+              if (css.media)
+                  { style.element.setAttribute('media', css.media); }
+              if (HEAD === undefined) {
+                  HEAD = document.head || document.getElementsByTagName('head')[0];
+              }
+              HEAD.appendChild(style.element);
+          }
+          if ('styleSheet' in style.element) {
+              style.styles.push(code);
+              style.element.styleSheet.cssText = style.styles
+                  .filter(Boolean)
+                  .join('\n');
+          }
+          else {
+              var index = style.ids.size - 1;
+              var textNode = document.createTextNode(code);
+              var nodes = style.element.childNodes;
+              if (nodes[index])
+                  { style.element.removeChild(nodes[index]); }
+              if (nodes.length)
+                  { style.element.insertBefore(textNode, nodes[index]); }
+              else
+                  { style.element.appendChild(textNode); }
+          }
+      }
+  }
+
+  /* script */
+  var __vue_script__ = script;
+
+  /* template */
+  var __vue_render__ = function() {
+    var _vm = this;
+    var _h = _vm.$createElement;
+    var _c = _vm._self._c || _h;
+    return _c(
+      "div",
+      { staticClass: "brand-card" },
+      [
+        _c("div", { staticClass: "btn-group" }, [_vm._t("btn-group")], 2),
+        _vm._v(" "),
+        _vm._t("default")
+      ],
+      2
+    )
+  };
+  var __vue_staticRenderFns__ = [];
+  __vue_render__._withStripped = true;
+
+    /* style */
+    var __vue_inject_styles__ = function (inject) {
+      if (!inject) { return }
+      inject("data-v-3528cfe4_0", { source: "\n.btn-group[data-v-3528cfe4] {\n  visibility: hidden;\n}\n.brand-card[data-v-3528cfe4] {\n&:hover {\n.btn-group {\n      visibility: visible;\n}\n}\n}\n", map: {"version":3,"sources":["/home/filsh/www/mailery/node_modules/@maileryio/mailery-brand-assets/src/components/BrandCard.vue"],"names":[],"mappings":";AAsBA;EACA,kBAAA;AACA;AAEA;AACA;AACA;MACA,mBAAA;AACA;AACA;AACA","file":"BrandCard.vue","sourcesContent":["<template>\n  <div class=\"brand-card\">\n    <div class=\"btn-group\">\n      <slot name=\"btn-group\"></slot>\n    </div>\n\n    <slot></slot>\n  </div>\n</template>\n\n<script>\n  export default {\n    name: 'ui-brand-card',\n    methods: {\n      handleHover() {\n        console.log(1)\n      }\n    }\n  }\n</script>\n\n<style scoped>\n  .btn-group {\n    visibility: hidden;\n  }\n\n  .brand-card {\n    &:hover {\n      .btn-group {\n        visibility: visible;\n      }\n    }\n  }\n</style>\n"]}, media: undefined });
+
+    };
+    /* scoped */
+    var __vue_scope_id__ = "data-v-3528cfe4";
+    /* module identifier */
+    var __vue_module_identifier__ = undefined;
+    /* functional template */
+    var __vue_is_functional_template__ = false;
+    /* style inject SSR */
+    
+    /* style inject shadow dom */
+    
+
+    
+    var __vue_component__ = normalizeComponent(
+      { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+      __vue_inject_styles__,
+      __vue_script__,
+      __vue_scope_id__,
+      __vue_is_functional_template__,
+      __vue_module_identifier__,
+      false,
+      createInjector,
+      undefined,
+      undefined
+    );
+
+  var plugin = {
+    install: install,
+    component: __vue_component__
+  };
+
+  (function (plugin) {
+    if (typeof window !== 'undefined' && window.Vue) {
+      Vue.use(plugin);
+    }
+  })(plugin);
+
+  function install(Vue, options) {
+    Vue.component(__vue_component__.name, __vue_component__);
+  }
+
+  exports.default = plugin;
+  exports.install = install;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=main.umd.js.map
---
+++ src/components/BrandCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="brand-card" v-on:mouseleave="handleMouseleave">
+    <slot></slot>
+
+    <div class="brand-card--btn-details">
+      <b-dropdown ref="dropdown" size="lg" variant="link" right no-caret>
+        <template v-slot:button-content>
+          <slot name="dropdown-button-content"></slot>
+        </template>
+        <slot name="dropdown-button-items"></slot>
+      </b-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ui-brand-card',
+    methods: {
+      handleMouseleave () {
+        this.$refs.dropdown.hide(true);
+      }
+    },
+  }
+</script>
+
+<style>
+  .dropdown-item-custom-link > .b-dropdown-text {
+    margin: 0;
+    padding: 0;
+  }
+
+  .dropdown-item-custom-link .btn-link {
+    width: 100%;
+    padding: .25rem 1.5rem;
+    text-align: left;
+  }
+</style>
+
+<style scoped>
+  .brand-card--btn-details {
+    /* display: none; */
+    position: absolute;
+    top: -0.5rem;
+    right: -0.7rem;
+    z-index: 1;
+  }
+
+  .brand-card {
+    position: relative;
+  }
+
+  .brand-card:hover .brand-card--btn-details {
+    display: block;
+  }
+</style>
